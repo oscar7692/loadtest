395553fa7c53051e52b94d74546499b7e7eadb06
Acosta Pulido Oscar (RBEI/EBS3.7-MX) <oscar.acostapulido@mx.bosch.com>
2020-07-28 10:35:33 -0500

    Adding wait time and stopwatch feature

--- loadtest/__init__.py
+++ loadtest/__init__.py
@@ -1,21 +1,32 @@
+from datetime import datetime, timedelta
+from dateutil import parser
 from queue import Queue
-from time import time
+from time import time, sleep
 from threading import Thread
-import requests as rq
+import json
+
+
+# globals
+json_log = []
 
 
 class LoadTester:
     def __init__(self,
+                 duration_time,
+                 request_time,
                  worker,
                  n_jobs: int = 30,
                  count: int = 10):
-        self.__n_jobs = n_jobs
-        self.__worker = worker
         self.__count = count
+        self.__d_time = datetime.now() + timedelta(minutes=int(duration_time)),
+        self.__d_time = parser.parse(str(self.__d_time[0]))
+        self.__n_jobs = n_jobs
         self.__q = Queue(self.__n_jobs * 2)
+        self.__request_time = request_time
         self.__tasks_duration_per_iteration = 0
-        self.__total_duration_per_task = 0
         self.__tasks_finished = 0
+        self.__total_duration_per_task = 0
+        self.__worker = worker
 
     def do_worker(self):
         while True:
@@ -53,9 +64,28 @@ class LoadTester:
 
                 iteration_duration = time() - start_iteration_time
                 self.__total_duration_per_task += self.__tasks_duration_per_iteration
+
                 print(f"iteration {iterations_count} finished in {iteration_duration:.2f}s, "
                       f"average thread duration is {self.__tasks_duration_per_iteration / self.__n_jobs:.2f}s, "
                       f"sum of thread durations is {self.__tasks_duration_per_iteration:.2f}s")
+
+                request_data = {
+                    "IterationNumber": f"{iterations_count}",
+                    "Values": {
+                        "RequestDone": f"{self.__count}",
+                        "AverageDurationPerRequest": f"{iteration_duration:.2f}s",
+                        "SumOfRequestDurations": f"{self.__tasks_duration_per_iteration:.2f}s"
+                        }
+                    }
+
+                json_log.append(request_data)
+                with open("request_log.json", "w+") as fl:
+                    fl.write(json.dumps(json_log, indent=2))
+
+                if self.__d_time < datetime.now():
+                    raise KeyboardInterrupt
+                sleep(self.__request_time)
+
         except KeyboardInterrupt:
             program_duration = time() - start_program_time
             average_task_duration = self.__total_duration_per_task / self.__tasks_finished \
